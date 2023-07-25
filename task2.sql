8419c850013ca23cb9074daa87a365e4e7902868
Rahmatjon <128359200+RahmatjonIbrohimov@users.noreply.github.com>
2023-07-25 17:52:44 +0500

    PostgreSQL hw2 done

---
+++ task2.sql
@@ -0,0 +1,281 @@
+INSERT INTO students (id, full_name, country, passport, university, date_of_birth, major) 
+VALUES (1, 'Merell Goncalo', 'France', null, 'Université Panthéon-Sorbonne (Paris I)', '2009-05-11', 'Technical Writer'),
+(2, 'Guenna Sollitt', 'Comoros', null, 'Nippon Bunri University', '2002-09-19', 'Structural Analysis Engineer'),
+(3, 'Amata Dugood', 'Germany', null, 'Beijing Petroleum University', '2002-04-30', 'Information Systems Manager'),
+(4, 'Ford Langford', 'Zimbabwe', null, 'Bindura University of Science Education', '2007-12-25', 'Desktop Support Technician'),
+(5, 'Myrle Bowering', 'Bulgaria', 'US', 'National Academy of Arts', '2007-01-05', 'Physical Therapy Assistant'),
+(6, 'Adelbert Griffiths', 'Spain', null, 'Open University of Spain', '2007-06-17', 'Desktop Support Technician'),
+(7, 'Bobbye Chatan', 'Germany', null, 'Northwest Polytechnical University Xi''an', '2001-09-28', 'Engineer II'),
+(8, 'Seana Oxley', 'Spain', null, 'Universitas Islam Jakarta', '2008-06-22', 'Assistant Professor'),
+(9, 'Wilt Stoter', 'Albania', null, 'University of Vlora "Ismail Qemali"', '2009-04-21', 'Social Worker'),
+(10, 'Alisa Repp', 'Guatemala', null, 'Universidad Rural de Guatemala', '2010-11-18', 'Software Engineer IV'),
+(11, 'Barny Jacobovitch', 'Germany', null, 'Wuxi University of Light Industry', '2008-02-13', 'Geological Engineer'),
+(12, 'Judon Hayden', 'Japan', null, 'Osaka Women''s University', '2003-12-01', 'Help Desk Operator'),
+(13, 'Faina Ianitti', 'Germany', null, 'Hohai University Changzhou', '2010-05-31', 'Nurse Practicioner'),
+(14, 'Timmi Cox', 'Spain', 'AB', 'Universidade de Caxias do Sul', '2007-10-03', 'Electrical Engineer'),
+(15, 'Fabe Tankard', 'Poland', 'AB', 'European Academy of Arts in Warsaw', '2006-12-08', 'Account Representative III'),
+(16, 'Willie Dobson', 'France', 'AB', 'Gettysburg College', '2001-03-17', 'Legal Assistant'),
+(17, 'Raff Milhench', 'Greece', 'AB', 'Technological Education Institute of Kavala', '2010-09-09', 'Senior Sales Associate'),
+(18, 'Nickolaus Hawksley', 'Norway', 'AB', 'Oslo School of Architecture', '2010-10-20', 'Database Administrator II'),
+(19, 'Concettina Scemp', 'Slovenia', 'AB', 'University of Nova Gorica', '2006-06-03', 'General Manager'),
+(20, 'Amaleta Lopes', 'Greece', 'AB', 'European University, Athens Campus', '2007-10-18', 'Electrical Engineer'),
+(21, 'Del Trusty', 'Germany', 'AB', 'Nanchang University', '2004-08-26', 'Dental Hygienist'),
+(22, 'Stormy Want', 'Spain', 'AB', 'Universitas Islam Riau', '2009-06-18', 'Speech Pathologist'),
+(23, 'Guy Marquot', 'Spain', 'AB', 'Universidade Federal do ABC', '2004-08-24', 'Recruiter'),
+(24, 'Winnie Farrall', 'Spain', 'AB', 'Novosibirsk State Medical Academy', '2000-01-01', 'Data Coordinator'),
+(25, 'Gaile Glayzer', 'Albania', 'AB', 'Epoka University', '2008-08-02', 'Senior Sales Associate'),
+(26, 'Cristin Gouthier', 'Poland', 'AB', 'Music Academy "Karol Szymanowski" in Katowice', '2006-05-11', 'VP Product Management'),
+(27, 'Hanson McMichan', 'Poland', 'AB', 'Academy of Economics in Poznan', '2002-01-11', 'Data Coordinator'),
+(28, 'Dylan O''Hegertie', 'Namibia', 'AB', 'Ilsa Independent College', '2009-04-05', 'Civil Engineer'),
+(29, 'Harlan Prestie', 'Philippines', 'AB', 'Wesleyan University Philippines', '2002-09-19', 'Director of Sales'),
+(30, 'Frankie Woolham', 'Guatemala', 'AB', 'Universidad del Istmo', '2010-02-08', 'Data Coordinator'),
+(31, 'Kilian Gladding', 'Peru', 'AB', 'Universidad Nacional de Ucayali', '2010-03-05', 'Structural Analysis Engineer'),
+(32, 'Kirk Luard', 'Honduras', null, 'Escuela Agricola Panamericana Zamorano', '2007-09-17', 'Design Engineer'),
+(33, 'Birdie Jaray', 'Poland', null, 'Music Academy "Fryderyk Chopin" in Warszaw', '2003-05-26', 'Chief Design Engineer'),
+(34, 'Sheena Andrieux', 'Germany', null, 'Yunnan University', '2003-11-17', 'Food Chemist'),
+(35, 'Elvira Magog', 'Bolivia', null, 'Universidad Domingo Savio', '2005-10-23', 'Office Assistant IV'),
+(36, 'Lane Simion', 'Croatia', null, 'University of Split', '2008-06-25', 'Food Chemist'),
+(37, 'Berky Votier', 'Sri Lanka', null, 'University of Moratuwa', '2006-10-30', 'Account Coordinator'),
+(38, 'Florencia Larrie', 'Portugal', null, 'Universidade do Algarve', '2006-02-26', 'Payment Adjustment Coordinator'),
+(39, 'Bruno Zanini', 'Peru', null, 'Universidad Privada Antenor Orrego', '2010-10-23', 'Product Engineer'),
+(40, 'Madella Stockings', 'Spain', 'AB', 'East-Siberian State University', '2007-07-13', 'Geologist III'),
+(41, 'Inesita Cowope', 'Japan', 'AB', 'Nara Women''s University', '2002-09-19', 'Quality Control Specialist'),
+(42, 'Olia Yearby', 'Spain', 'AB', 'Kalmyk State University', '2005-12-03', 'Nurse Practicioner'),
+(43, 'Tiffi Vallack', 'Spain', 'AB', 'Universitas Khairun', '2001-04-21', 'Graphic Designer'),
+(44, 'Dore Weller', 'Belarus', 'AB', 'Gomel State Technical University Pavel Sukhoi', '2002-07-31', 'Associate Professor'),
+(45, 'Hannie McRill', 'Sweden', 'AB', 'Lund University', '2007-09-05', 'Information Systems Manager'),
+(46, 'Minny Warbey', 'Vietnam', null, 'Hanoi University of Civil Engineering', '2006-08-19', 'Senior Developer'),
+(47, 'Arnuad Brydone', 'Guatemala', null, 'Universidad de San Carlos de Guatemala', '2009-12-07', 'Health Coach II'),
+(48, 'Jeffrey Wylam', 'United Kingdom', null, 'Goldsmiths College, University of London', '2004-11-21', 'GIS Technical Architect'),
+(49, 'Inness Hatfull', 'Germany', null, 'Urumqi Vocational University', '2008-11-14', 'Human Resources Assistant I'),
+(50, 'Demetri Zambonini', 'Slovenia', null, 'University of Ljubljana', '2000-06-27', 'Internal Auditor'),
+(51, 'Orbadiah Guiso', 'Portugal', null, 'Universidade de Aveiro', '2004-11-01', 'Paralegal'),
+(52, 'Eleonora Foux', 'France', null, 'City University of New York, Medgar Evers College', '2010-06-19', 'Business Systems Development Analyst'),
+(53, 'Horatio Burrells', 'Bulgaria', null, 'National Academy for Theatre and Film Arts "Krustju Sarafov"', '2006-05-26', 'Product Engineer'),
+(54, 'Bliss Bisseker', 'Germany', null, 'Central South University', '2001-03-20', 'Staff Accountant I'),
+(55, 'Tiffie McFaell', 'Germany', null, 'Xinjiang Normal University', '2005-06-23', 'Nurse Practicioner'),
+(56, 'Barron O''Collopy', 'Spain', null, 'Universidade de Brasília', '2009-12-27', 'Assistant Professor'),
+(57, 'Adina Whanstall', 'Germany', null, 'Beijing Normal University', '2007-01-13', 'Analog Circuit Design manager'),
+(58, 'Ranice Dionisetto', 'Philippines', null, 'Northwestern University of the Philippines', '2005-08-31', 'Product Engineer'),
+(59, 'Ruprecht Helstrip', 'Germany', null, 'Xi''an University of Architecture and Technology', '2000-01-25', 'Technical Writer'),
+(60, 'Roxie Joska', 'Philippines', null, 'Northwestern University of the Philippines', '2005-02-19', 'Design Engineer');
+
+
+-- 
+-- 
+-- 
+
+INSERT INTO people (id, first_name, date_of_birth, passport, country, is_married) 
+VALUES (2, 'Loella', '2016/06/14', 'US', 'Peru', true),
+(3, 'Anetta', '1987/09/23', 'HN', 'Peru', true),
+(4, 'Don', '1998/09/30', 'CO', 'Ukraine', false),
+(5, 'Ginevra', '1987/04/25', 'CA', 'Iran', false),
+(6, 'Eleanora', '2005/07/12', 'PE', 'Thailand', false),
+(7, 'Rachael', '1999/10/23', 'BD', 'Poland', true),
+(8, 'Wright', '1985/07/05', 'US', 'Indonesia', false),
+(9, 'Stevana', '2017/11/07', 'NG', 'China', false),
+(10, 'Erik Julie', '2012/06/02', 'BR', 'Guatemala', true),
+(34, 'Erik Shayla', '1992/11/19', 'AU', 'Indonesia', true),
+(35, 'Shandee', '2000/11/01', 'US', 'China', true),
+(36, 'Leicester', '1983/06/21', 'KI', 'Macedonia', true),
+(37, 'Gwenneth', '2006/04/09', 'ID', 'Sweden', false),
+(38, 'Marchelle', '1984/04/23', 'ID', 'Philippines', true),
+(39, 'Adam', '1994/08/29', 'AU', 'Bangladesh', false),
+(40, 'Silvan', '1981/02/24', 'VU', 'Colombia', true),
+(41, 'Cassandry', '2002/01/03', 'NZ', 'China', false),
+(42, 'Erik Clerissa', '2012/07/12', 'CA', 'Thailand', true),
+(43, 'Erik Caren', '2019/05/11', 'AU', 'Armenia', true),
+(44, 'Erik Warden', '1987/02/08', 'KZ', 'Guatemala', true),
+(45, 'Erik Florry', '2001/06/03', 'PG', 'Malaysia', false),
+(46, 'Erik Ibby', '2011/12/24', 'US', 'Australia', false),
+(47, 'Elnar', '1984/11/07', 'US', 'Poland', false),
+(48, 'Colver', '2003/01/24', 'VN', 'Brazil', false),
+(49, 'Marcelle', '2008/09/28', 'AW', 'Colombia', false),
+(50, 'Hayder Albie', '2005/12/25', 'IR', 'Czech Republic', false),
+(51, 'Hayder Modestine', '2005/05/30', 'US', 'China', true),
+(52, 'Hayder Harley', '2001/11/15', 'BR', 'Ukraine', false),
+(53, 'Hayder Reeva', '2009/07/18', 'US', 'Mexico', false),
+(54, 'Hayder Neville', '2010/11/01', 'JP', 'Afghanistan', false),
+(55, 'Bibby', '1997/04/05', 'CN', 'China', false),
+(56, 'Isabelle', '2001/09/20', 'CG', 'China', false),
+(57, 'Hayder Ambrose', '1991/06/10', 'ET', 'Gambia', true),
+(58, 'Yancy', '1999/07/02', 'AR', 'China', false),
+(59, 'Marius', '2011/06/25', 'VU', 'Japan', false),
+(60, 'Danette', '2000/07/26', 'US', 'United States', false),
+(61, 'Peggie', '2020/06/20', 'CA', 'Brazil', true),
+(62, 'Ashlie', '1989/03/12', 'CN', 'Philippines', true),
+(63, 'Bram', '1980/11/18', 'US', 'France', true),
+(64, 'Krystal', '2009/03/27', 'MH', 'China', true),
+(65, 'Feliza', '1982/01/09', 'AU', 'Indonesia', false),
+(66, 'Glad', '2019/06/03', 'US', 'Poland', false),
+(67, 'Justin', '1981/12/28', 'ZA', 'Czech Republic', true),
+(68, 'Sydelle', '2010/03/03', 'CN', 'China', true),
+(69, 'Jeanna', '2017/05/16', 'GB', 'China', true),
+(70, 'Mayer', '1983/10/28', 'RU', 'China', false),
+(71, 'Starlin', '2007/05/16', 'US', 'Czech Republic', false),
+(72, 'Maren', '2000/08/13', 'NO', 'Lesotho', true),
+(73, 'Erika', '2013/02/17', 'BR', 'Sweden', true),
+(74, 'Raimundo', '2002/11/12', 'IN', 'China', true),
+(75, 'Gillie', '2005/02/12', 'GB', 'Russia', true),
+(76, 'Demetri', '1995/03/05', 'ID', 'Japan', true),
+(77, 'Garey', '2009/08/19', 'IN', 'China', false),
+(78, 'Bert', '1986/12/02', 'US', 'United States', false),
+(79, 'Belva', '1985/02/21', 'MA', 'Poland', false),
+(80, 'Alexandra', '2001/10/27', 'IN', 'China', false),
+(81, 'Carey', '1989/11/22', 'US', 'Tanzania', false),
+(82, 'Alf', '2003/10/25', 'CN', 'Indonesia', false);
+
+
+-- 
+-- 
+-- 
+
+INSERT INTO products (name, price, made_in, company, type) 
+VALUES ('Iphone -15 ultra', 1, 'Bulgaria', 'apple', 'vegetables'),
+('Coconut - Creamed, Pure', 94, 'Colombia', 'Mita', 'diary'),
+('Thermometer Digital', 31, 'Comoros', 'Realbridge', 'chocolate'),
+('Transfer Sheets', 53, 'Philippines', 'Meevee', 'fruit'),
+('Plasticspoonblack', 92, 'Pakistan', 'Bubbletube', 'instapayment'),
+('Wine - Cousino Macul Antiguas', 3, 'Philippines', 'Jayo', 'diary'),
+('Shrimp - 31/40', 75, 'Poland', 'Zazio', 'diary'),
+('Bread - Multigrain', 37, 'Poland', 'Buzzbean', 'vegetables'),
+('Foie Gras', 28, 'France', 'Shufflester', 'fruit'),
+('Rice Pilaf, Dry,package', 600, 'China', 'Snaptags', 'diary'),
+('Gloves - Goldtouch Disposable', 29, 'Afghanistan', 'Fiveclub', 'fruit'),
+('Ice - Clear, 300 Lb For Carving', 96, 'Indonesia', 'Jabberstorm', 'fruit'),
+('Ipad s21 max', 41, 'Dominican Republic', 'apple', 'diary'),
+('Wine - Jaboulet Cotes Du Rhone', 76, 'Bangladesh', 'Tagchat', 'instapayment'),
+('Energy Drink - Redbull 6005ml', 37, 'France', 'Browsetype', 'diary'),
+('Cookie Trail Mix', 600, 'Poland', 'Feedspan', 'diary'),
+('MacBook Surf', 34, 'Uruguay', 'apple', 'vegetables'),
+('Corn Kernels - Frozen', 600, 'Thailand', 'Vipe', 'diners-club-carte-blanche'),
+('Vinegar - Champagne', 13, 'Indonesia', 'Voonix', 'fruit'),
+('Baking Soda', 12, 'Sweden', 'Livefish', 'diary'),
+('Absolut Citron', 47, 'China', 'Tagpad', 'diary'),
+('Cheese - Manchego, Spanish', 44, 'China', 'InnoZ', 'fruit'),
+('Beef - Montreal Smoked Brisket', 89, 'Czech Republic', 'Kayveo', 'diary'),
+('Cheese - Parmesan Cubes', 28, 'Sweden', 'Trilith', 'diary'),
+('Iphone 18', 58, 'Serbia', 'apple', 'vegetables'),
+('Ihone s23 pro max', 96, 'Browsebug', 'apple', 'fruit'),
+('Veal - Loin', 96, 'Philippines', 'Topiclounge', 'chocolate'),
+('Anchovy In Oil', 26, 'Russia', 'Quimba', 'diary'),
+('Soupfoamcont12oz 6002con', 14, 'China', 'Trupe', 'chocolate'),
+('Icecream - Dstk Cml And Fdg', 64, 'China', 'Gigashots', 'diary'),
+('Wine - Red, Pelee Island Merlot', 78, 'Peru', 'BlogXS', 'fruit'),
+('Lamb Shoulder Boneless Nz', 62, 'Brazil', 'Eayo', 'diary'),
+('Lychee - Canned', 600, 'China', 'Trilia', 'vegetables'),
+('Wine - Semi Dry Riesling Vineland', 600, 'China', 'Zoomzone', 'chocolate'),
+('Sugar - Individual Portions', 76, 'Peru', 'Yombu', 'diary'),
+('Pork - Liver', 94, 'Indonesia', 'Abatz', 'diary'),
+('Nut - Cashews, Whole, Raw', 83, 'Russia', 'Muxo', 'diary'),
+('Cheese - Cheddar, Mild', 84, 'Afghanistan', 'Photobean', 'diary'),
+('Gelatine Leaves - Envelopes', 42, 'Russia', 'Kwideo', 'diary'),
+('iMac 12', 600, 'Canada', 'apple', 'fruit'),
+('Loaf Pan - 2 Lb, Foil', 22, 'Peru', 'Blogpad', 'chocolate'),
+('Versatainer Nc - 8288', 81, 'Iran', 'Cogibox', 'fruit'),
+('Wine - Chianti Classico Riserva', 88, 'China', 'Shuffledrive', 'fruit'),
+('Cheese - Cream Cheese', 60, 'Sweden', 'Livefish', 'diary'),
+('Fish - Scallops, Cold Smoked', 38, 'Russia', 'Roomm', 'diary'),
+('Sterno - Chafing Dish Fuel', 48, 'Norway', 'Brainlounge', 'fruit'),
+('Loquat', 91, 'Madagascar', 'Agimba', 'chocolate'),
+('Sauce - Hollandaise', 72, 'Jordan', 'Gabvine', 'diary');
+
+-- 
+-- 
+-- 
+
+INSERT INTO films (id, title, genre, duration, rate, country, author)
+VALUES (1, 'I Don''t Know How She Does It', 'Comedy', 71, 6, 'Bulgaria', 'Red'),
+(2, 'Raw Deal: A Question of Consent', 'Documentary', 10, 9, 'Guatemala', 'Violet'),
+(4, 'Bronx Tale, A', 'Drama', 59, 1, 'Czech Republic', 'Khaki'),
+(5, 'Year of the Dog', 'Comedy|Drama|Romance', 38, 9, 'Nigeria', 'Fuscia'),
+(6, 'Genesis', 'Drama|Horror', 99, 3, 'turkistan', 'Turquoise'),
+(7, 'Lawnmower Man, The', 'Action', 25, 9, 'Russia', 'Indigo'),
+(8, 'Sabata', 'Action', 70, 9, 'Pakistan', 'Green'),
+(9, 'Inspector General, The', 'Musical', 30, 2, 'turkistan', 'Aquamarine'),
+(10, 'Paranormal Activity', 'Horror', 82, 6, 'kazakhstan', 'Blue'),
+(11, 'Flight from Death: The Quest for Immortality', 'Documentary', 86, 1, 'France', 'Goldenrod'),
+(12, 'Blue Steel', 'Action', 45, 10, 'Argentina', 'Aquamarine'),
+(13, 'Rabbit', 'Horror', 29, 10, 'turkistan', 'Green'),
+(15, 'Big Hangover, The', 'Comedy', 58, 10, 'Nigeria', 'Goldenrod'),
+(16, 'Time Walker (a.k.a. Being From Another Planet)', 'Horror', 77, 8, 'turkistan', 'Goldenrod'),
+(17, 'Boynton Beach Bereavement Club, The', 'Comedy|Drama', 11, 9, 'turkistan', 'Aquamarine'),
+(18, 'Silja - nuorena nukkunut', 'Drama|Romance', 54, 5, 'Ukraine', 'Fuscia'),
+(19, 'Stranger by the Lake (L''inconnu du lac)', 'Drama|Romance', 45, 2, 'Guatemala', 'Khaki'),
+(20, 'Breed Apart, A', 'Action', 77, 7, 'turkistan', 'Goldenrod'),
+(21, 'Lisa Picard is Famous (a.k.a. Famous)', 'Comedy|Drama', 37, 10, 'uzbekistan', 'Blue'),
+(22, 'Kurt Cobain About a Son', 'Documentary|Musical', 46, 8, 'Sweden', 'Aquamarine'),
+(23, 'Quod erat demonstrandum', 'Drama', 14, 9, 'turkistan', 'Purple'),
+(24, 'Black Magic Rites & the Secret Orgies of the 14th Century (Riti, magie nere e segrete orge nel trecento...)', 'Horror', 57, 8, 'Portugal', 'Green'),
+(25, 'Ballad of the Sad Cafe, The', 'Drama', 13, 10, 'Sweden', 'Green'),
+(26, 'Krrish 3', 'Action', 69, 7, 'Russia', 'Pink'),
+(27, 'Parking Lot Movie, The', 'Documentary', 27, 8, 'uzbekistan', 'Crimson'),
+(28, 'Love Potion #9', 'Comedy|Romance', 77, 2, 'Bahamas', 'Khaki'),
+(29, '6 Souls (Shelter)', 'Horror', 89, 10, 'Finland', 'Green'),
+(30, 'Riot Club, The', 'Drama|Thriller', 75, 9, 'Brazil', 'Violet'),
+(31, 'Out of Life (Hors la vie)', 'Drama', 32, 9, 'turkistan', 'Blue'),
+(32, 'Cyrus', 'Comedy|Drama|Romance', 95, 10, 'turkistan', 'Yellow'),
+(33, 'Henry Fool', 'Horror', 32, 1, 'United States', 'Pink'),
+(34, 'The Sex and Violence Family Hour', '(no genres listed)', 54, 7, 'Poland', 'Mauv'),
+(35, 'Captain America: The First Avenger', 'Action', 61, 1, 'Germany', 'Yellow'),
+(36, 'Across 110th Street', 'Crime', 64, 5, 'Poland', 'Violet'),
+(37, 'Houseguest', 'Comedy', 65, 5, 'kazakhstan', 'Mauv'),
+(38, 'Young Doctors in Love', 'Comedy', 17, 9, 'France', 'Violet'),
+(39, 'Spider', 'Action', 96, 4, 'turkistan', 'Blue'),
+(40, 'A Year Along the Abandoned Road', '(no genres listed)', 65, 2, 'Vietnam', 'Turquoise'),
+(41, 'Cats', 'Musical', 21, 10, 'Czech Republic', 'Fuscia'),
+(42, 'Last King of Scotland, The', 'Drama|Thriller', 97, 1, 'turkistan', 'Maroon'),
+(43, 'Out 1: Spectre', 'Comedy|Drama|Thriller', 52, 7, 'kazakhstan', 'Mauv'),
+(44, 'Death Note 2: The Last Name', 'Adventure|Crime|Drama', 65, 9, 'Ukraine', 'Maroon'),
+(45, 'Kingdom, The', 'Action', 58, 10, 'Pakistan', 'Puce'),
+(46, 'Stone Reader', 'Documentary', 57, 6, 'Pakistan', 'Mauv'),
+(47, '100 Ways to Murder Your Wife (Sha qi er ren zu)', 'Comedy', 48, 9, 'Russia', 'Red'),
+(48, 'Applause (Applaus)', 'Drama', 28, 10, 'Greece', 'Yellow'),
+(49, 'Lupin III: First Contact (Rupan Sansei: Faasuto Kontakuto)', 'Action|Animation|Comedy|Crime|Drama|Mystery|Romance|Thriller', 24, 8, 'turkistan', 'Blue'),
+(50, 'Game of Death, The (Le Jeu de la Mort)', 'Documentary', 25, 2, 'Argentina', 'Orange'),
+(51, 'The Thirteen Assassins', 'Action', 31, 5, 'Russia', 'Blue'),
+(52, 'Men of Respect', 'Crime|Drama|Thriller', 44, 2, 'Iran', 'Red'),
+(53, 'Light Years (Gandahar)', 'Adventure|Animation|Fantasy|Sci-Fi', 33, 5, 'Portugal', 'Crimson'),
+(54, 'Phenix City Story, The', 'Crime|Drama|Film-Noir', 38, 4, 'turkistan', 'Crimson'),
+(55, 'Cold Fish (Tsumetai nettaigyo)', 'Drama|Thriller', 30, 4, 'turkistan', 'Fuscia');
+
+-- 
+-- 
+
+SELECT full_name, date_of_birth, major, university, country FROM students;
+SELECT id, first_name, date_of_birth, passport, country, is_married FROM people;
+SELECT name, price, made_in, company, type FROM products;
+SELECT id, title, genre, duration, rate, country, author FROM films;
+
+
+-- 2.2
+SELECT * FROM students WHERE country = 'Germany';
+SELECT * FROM students WHERE country = 'France';
+SELECT * FROM students WHERE country = 'Spain';
+SELECT * FROM students WHERE major LIKE '%Engineer%';
+SELECT * FROM people WHERE is_married = 'true';
+SELECT * FROM students WHERE date_of_birth < '2015-12-31';
+SELECT * FROM products WHERE type = 'diary';
+SELECT * FROM products WHERE type = 'fruit';
+SELECT * FROM products WHERE type = 'chocolate';
+SELECT * FROM products WHERE company = 'apple';
+SELECT * FROM films WHERE genre = 'Action';
+SELECT * FROM films WHERE genre = 'Horror';
+SELECT * FROM films WHERE rate > 4 and rate < 8;
+
+
+-- 2.3
+SELECT * FROM students WHERE passport LIKE 'AB%';
+SELECT * FROM people WHERE first_name LIKE 'Erik%';
+SELECT * FROM products WHERE price = 600;
+SELECT * FROM films WHERE country LIKE '%stan';
+
+
+
+
+
+
+
+
+
+
